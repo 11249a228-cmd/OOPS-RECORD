96a96647d400e0fcdd7637c06905e1e622cfd7af
Monish MSM <11249a228@kanchiuniv.ac.in>
2025-11-04 21:07:54 -0800

    Add files via upload

--- book1.cpp
+++ book1.cpp
@@ -1,40 +1,47 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Book
-{
-    private:
-    int pages; int pages;
-    char Bookname[50],ISBNO[50],Author[50];
+
+class Book {
+private:
+    int pages;
+    char Bookname[50], ISBNO[50], Author[50];
     float price;
-    public:
+    int AccNo;
+
+public:
     void getdata();
     void display();
 };
-void::getdata()
-{
-cout<<"Enter the Book AccNo:";
-cin>>AccNo;
-cout<<"Enter the Book pages:";
-cin>>pages;
-cout<<"Enter the Book Name:";
-cin>>Bookname;
-cout<<"Enter the Book Author:";
-cin>>Author;
-cout<<"Enter the Book Price:";
-cin>>price;
+
+void Book::getdata() {
+    cout << "Enter the Book AccNo: ";
+    cin >> AccNo;
+    cout << "Enter the number of pages: ";
+    cin >> pages;
+    cout << "Enter the Book Name: ";
+    cin.ignore(); // clear input buffer before getline
+    cin.getline(Bookname, 50);
+    cout << "Enter the Author Name: ";
+    cin.getline(Author, 50);
+    cout << "Enter the Book ISBN No: ";
+    cin.getline(ISBNO, 50);
+    cout << "Enter the Book Price: ";
+    cin >> price;
 }
-void::display()
-{
-    cout<<"Book AccNo"<<AccNo;
-    cout<<"Book Name"<<Name;
-    cout<<"Book ISBNO"<<IsbNo;  
-    cout<<"Book Author"<<Author;
-    cout<<"No.of pages in Book"<<pages;
-    cout<<"price of Book"<<price;
+
+void Book::display() {
+    cout << "\nBook Details:\n";
+    cout << "AccNo: " << AccNo << endl;
+    cout << "Name: " << Bookname << endl;
+    cout << "ISBN No: " << ISBNO << endl;
+    cout << "Author: " << Author << endl;
+    cout << "Pages: " << pages << endl;
+    cout << "Price: " << price << endl;
 }
-int main(){zzx
+
+int main() {
     Book s;
     s.getdata();
     s.display();
-
+    return 0;
 }
