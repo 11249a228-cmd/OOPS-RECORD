b4e6587220e6c44f0fbf3239e11e2f75e654d88a
Monish S.M <11249a228@kanchiuniv.ac.in>
2025-11-07 21:52:44 -0800

    Update multilevel.cpp

--- multilevel.cpp
+++ multilevel.cpp
@@ -1,31 +1,87 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-class A {
+class Student {
+private:
+    string name;
+    int rno;
+
 public:
-    void showA() {
-        cout << "Class A" << endl;
+    void getStudent() {
+        cout << "Enter name of the student: ";
+        cin >> name;
+        cout << "Enter roll number of the student: ";
+        cin >> rno;
+    }
+
+    void displayStudent() const {
+        cout << "Name of the student: " << name;
+        cout << "\nRoll number of the student: " << rno;
     }
 };
 
-class B : public A {
+class Test : public Student {
+protected:
+    int math, eng, sci;
+
 public:
-    void showB() {
-        cout << "Class B" << endl;
+    void getTest() {
+        getStudent();
+        cout << "Enter Math marks: ";
+        cin >> math;
+        cout << "Enter English marks: ";
+        cin >> eng;
+        cout << "Enter Science marks: ";
+        cin >> sci;
+    }
+
+    void displayTest() const {
+        displayStudent();
+        cout << "\nMath marks: " << math;
+        cout << "\nEnglish marks: " << eng;
+        cout << "\nScience marks: " << sci;
     }
 };
 
-class C : public B {
+class Result : public Test {
+private:
+    int total;
+    float avg;
+
 public:
-    void showC() {
-        cout << "Class C" << endl;
+    void getResult() {
+        getTest();
+        total = math + eng + sci;
+        avg = total / 3.0f;
+    }
+
+    void displayResult() const {
+        displayTest();
+        cout << "\nTotal Marks: " << total;
+        cout << "\nAverage Marks: " << avg;
     }
 };
 
 int main() {
-    C obj;
-    obj.showA();
-    obj.showB();
-    obj.showC();
+    int n;
+    cout << "Enter number of students: ";
+    cin >> n;
+
+    Result students[n];  // Array of Result objects
+
+    cout << "\n--- Enter details for each student ---\n";
+    for (int i = 0; i < n; ++i) {
+        cout << "\nStudent " << i + 1 << ":\n";
+        students[i].getResult();
+    }
+
+    cout << "\n\n--- Student Results ---\n";
+    for (int i = 0; i < n; ++i) {
+        cout << "\nStudent " << i + 1 << ":\n";
+        students[i].displayResult();
+        cout << "\n----------------------\n";
+    }
+
     return 0;
 }
